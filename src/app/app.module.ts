0e416e9f2577a9c0183912908562177ca43f1f56
HarmonyOrakpo <harmonyorakpo@gmail.com>
2024-01-10 13:17:44 +0100

    resolved issue with

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { SharedModule } from './shared/shared.module';
     BrowserAnimationsModule,
     AppRoutingModule,
     SharedModule,
+    HomeModule
   ],
   providers: [
     provideClientHydration()
