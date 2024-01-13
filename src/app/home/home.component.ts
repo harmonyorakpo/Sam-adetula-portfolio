3f6a914f4da6b9706dd2ea4ca79555c31fc8a932
HarmonyOrakpo <harmonyorakpo@gmail.com>
2024-01-13 23:40:35 +0100

    completed the design for the home screen

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,4 +15,28 @@ export class HomeComponent implements OnInit {
   headerImageDescription = 'spaceship';
 
   ngOnInit(): void {}
+
+  cardContent = [
+    {
+      title: 'Skills',
+      description:
+        'Mobile and Web app design, Reponsive web design, User-Centric Design, User Research and Interviews, Competitve Analysis, Synthesizing Insights, Advanced Prototyping, Wireframing, Usability Testing.',
+      image: '/assets/Home page assets/Skills image.svg',
+      imageDescription: 'Skills image',
+    },
+    {
+      title: 'Tools',
+      description:
+        'I use Figma and Adobe XD for Interface design, for User Interview and Testing, I use Maze. To fuel inspiration, I explore other live products (App or web) as well as platforms like Awwwards, Behance, Dribbble, and Google.',
+      image: '/assets/Home page assets/Tools image.svg',
+      imageDescription: 'Tools image',
+    },
+    {
+      title: 'Education',
+      description:
+        "I hold a bachelor's degree in Electrical Electronics Engineering, and Google UX Design Certificate from Coursera. Additionally, I've obtained UX and UI Design certificates from platforms such as Udemy and Udacity.",
+      image: '/assets/Home page assets/Education image.svg',
+      imageDescription: 'Education image',
+    },
+  ];
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
       spacing: {
-        '100':'26rem',
-        '114':'30rem',
-        '128': '32rem',
-        '136': '34rem',
+        100: "26rem",
+        114: "30rem",
+        128: "32rem",
+        136: "34rem",
+      },
+      colors: {
+        cardBorder: "#141414",
+        sunEffect: "#FFAD0F"
+      },
+      blur: {
+        '4xl': '72px',
+        '5xl': '84px',
+        '6xl': '96px',
       }
     },
   },
   plugins: [],
-}
+};
